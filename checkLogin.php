d60db5d00f88cac0034a50c79546374b1d32afad
Shubham Kumar <shubham.iitp@gmail.com>
2013-11-09 12:46:38 +0530

    Initial commit

---
+++ checkLogin.php
@@ -0,0 +1,18 @@
+<?php
+	include('config.inc');
+?>
+
+<?php 
+	echo 'jai Sri ram';
+?>
+
+<html>
+	<head>
+		<title>
+			<p> <?php echo $_POST['user_name']; ?> </p>
+		</title>
+	</head>
+	<body>
+	<?php echo '<p>Hello World</p>'; ?> 
+	</body>
+</html>
